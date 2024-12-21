b92b63e71f0175c7366728c2758869e24ec173f0
Larry <larry@lltweed.com>
2024-12-21 15:09:05 -0800

    added entrance and exit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import ctypes
 import sys
 from graphics import Window, Line, Point
 from cell import Cell
+from maze import Maze
 
 # Set DPI awareness (Windows only)
 if sys.platform == "win32":  # Check if the platform is Windows
@@ -14,41 +15,16 @@ if sys.platform == "win32":  # Check if the platform is Windows
 
 def main():
     # Create a window instance
-    win = Window(1600, 1200)  # Use a larger resolution
-    if win is None:
-        print("Failed to create a window.")
-        return
-    print("Window created...")
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
-    print("Cell 1 created...")
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 1600
+    screen_y = 1200
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-    print("Cell 2 created...")
-
-    c1.draw_move(c2)
-    print("Cell 1 moved to Cell 2...")
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-    print("Cell 3 created...")
-
-    c2.draw_move(c3)
-    print("Cell 2 moved to Cell 3...")
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-    print("Cell 4 created...")
-
-    c3.draw_move(c4, True)
-    print("Cell 3 moved to Cell 4...")
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 if __name__ == "__main__":
---
+++ maze.py
@@ -0,0 +1,54 @@
+from cell import Cell
+import time
+import random
+
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._cells = []
+        self._win = win
+
+        self._create_cells(win)
+        self._break_entrance_and_exit()
+
+    def _create_cells(self, win):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+            #print(f"{len(self._cells)}\n")
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            #print("Window is not set.")
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self.animate()
+
+    def animate(self):
+        if self._win is not None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        print(f"_cells[0][0].has_top_wall: {self._cells[0][0].has_top_wall}")
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+    
